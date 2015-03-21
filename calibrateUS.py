5a5170422726adefb65385aa54321458a7a681d5
Aaron S. <thespacex101@gmail.com>
2015-03-20 23:11:02 -0400

    Fixes and edits for calibrateUS

--- calibrateUS.py
+++ calibrateUS.py
@@ -1,13 +1,13 @@
-from .ev3dev import LegoSensor, Motor
+from ev3.lego import LegoSensor, Motor, ColorSensor
 import time
 
 #Sensor Init
-colorL = ColorSensor(port=0)
-colorR = ColorSensor(port=1)
-gyro = ColorSensor(port=2)
-ultrasonic = ColorSensor(port=3)
+colorL = ColorSensor(port=1)
+colorR = ColorSensor(port=2)
+gyro = ColorSensor(port=3)
+ultrasonic = ColorSensor(port=4)
 USAngle = 0
-USDist = 360			#Absolute amount to turn US 360 degrees
+USDist = 2550		#Absolute amount to turn US 360 degrees
 
 #MotorInit
 def init_motor(motor):
@@ -27,22 +27,28 @@ c = Motor(port=Motor.PORT.C)
 init_motor(a)
 init_motor(b)
 init_motor(c)
-a.pulses_per_second_sp = 2000
-b.pulses_per_second_sp = 2000
-c.pulses_per_second_sp = 2000
-time.sleep(5)
+#a.pulses_per_second_sp = 2000
+#b.pulses_per_second_sp = 2000
+#c.pulses_per_second_sp = 2000
+#time.sleep(5)
 a.pulses_per_second_sp = 0
 b.pulses_per_second_sp = 0
-c.pulses_per_second_sp = 0
+c.pulses_per_second_sp = 2000
 defaultSpeed = 1000
+a.stop()
+b.stop()
+c.stop()
 
 def calibrateUS():
 	turnUS(360)
 
 def turnUS(angle):
-c.run_position_limited(100,(angle/360.0)*USDist)
-USAngle += angle
-if(USAngle < 0)
-	USAngle += 360
-elif(USAngle > 360)
-	USAngle -= 360
+	c.run_position_limited((angle/360.0)*USDist,1000)
+	global USAngle
+	USAngle += angle
+	if(USAngle < 0):
+		USAngle += 360
+	elif(USAngle > 360):
+		USAngle -= 360
+
+calibrateUS()
